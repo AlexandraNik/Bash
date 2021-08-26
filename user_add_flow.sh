1931855c9f3ebd740c052a5f7687a38dcc6abf64
purple-thistle <sashan@vaiolabs.com>
2021-08-26 16:03:51 +0300

    adding features

--- user_add_flow.sh
+++ user_add_flow.sh
@@ -14,15 +14,44 @@ if [[  "$UID" != "0" ]]; then
 	exit 1
 
 else
+	#username
 	read -p "Enter a user name: " username
-	adduser "$username"
-
+	
+	res=$(cat /etc/passwd | grep "$username")
+	if [[ -z "$res" ]]; then
+		useradd "$username"
+	else
+		echo "User already exists"
+		exit 1
+	fi	
+	
+	
+	
+	#password
 	read -s -p "Enter a password: " password
-	echo "$password" | passwd --stdin "$username"
+	
+	while [[ ${#password} < 6 ]]; do
+		read -s -p "Password should be at least 6 characters long. Try again: " password
+	done		
 
+	echo "$password" | passwd --stdin "$username"
+	
+
+	#group
 	read -p "Enter a group name: " gname
+	
+	res=$(cat /etc/group | grep "$gname")
+	if [[ -z "$res" ]]; then
+		read -p "Group does not exist. To create press 'y'." ans
+		if [[ "$ans" == "y" ]]; then
+			groupadd "$gname"
+		fi	
+	fi	
+
         usermod -a -G "$gname" "$username"
 
+
+	#home dir
 	read -p "Choose home directory: " hdir
 	usermod -m -d "$hdir" "$username"
 
